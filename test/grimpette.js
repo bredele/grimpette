00d517fc5240596070fffad5286abf00ad2621d4
bredele <olivier.wietrich@gmail.com>
2020-02-08 15:16:35 -0700

    Returns root path

--- test/grimpette.js
+++ test/grimpette.js
@@ -6,7 +6,7 @@ const test = require('tape')
 const grimpette = require('..')
 
 
-test('should return the root route', assert => {
+test('should return root path', assert => {
   assert.plan(1)
   const router = grimpette()
   assert.equal(router(), '/')
